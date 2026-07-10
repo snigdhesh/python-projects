fe38a5bcb3500cb92f7a7eeb082b90d59d3f18e4
snigdhesh <snigdhesh@live.com>
2026-07-09 20:51:53 -0400

    using datetime

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
-def calculateSalary(hoursWorked, hourlyPay):
+import datetime
+
+def getEmployeeSalary(hoursWorked, hourlyPay):
     return hoursWorked * hourlyPay
 
-manasaSalary = calculateSalary(40, 15) #manasa
-divyaSalary = calculateSalary(35, 20) #divya
+manasaSalary = getEmployeeSalary(40, 15) #manasa
+divyaSalary = getEmployeeSalary(35, 20) #divya
 
 print(f"manasa earned {manasaSalary}")
 print(f"Divya earned {divyaSalary}")
+
+def getEmployeeInfo(employeeId):
+    if(employeeId == 1001):
+        return '{"name": "Manasa", "email": "manasa@gmail.com"}'
+    elif(employeeId == 1002):
+        return '{"name": "Divya", "email": "divya@gmail.com"}'
+    else:
+        return 'No user found'
+
+
+user = getEmployeeInfo(1003)
+print(user)
+
+
+def getSchoolStatus(currentTime):
+    if 8 <= currentTime.hour < 18:
+        return 'school is open'
+    else:
+        return 'school is closed'
+
+
+status = getSchoolStatus(datetime.datetime.now().time())
+print(status)
